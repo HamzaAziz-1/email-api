6d41d057d5a3e18b50dcb63c172dd5968436e428
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-03-25 00:36:05 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ router.post("/send-email", async (req, res) => {
     }
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-      to: "fa19-bse-001@cuilahore.edu.pk", // Change to your recipient
+      to: "hamzaaaziz001@gmail.com", // Change to your recipient
       from: 'Boom <hamza.aziz8750@gmail.com>',
       subject: `New message from ${name}`,
       text: message,
